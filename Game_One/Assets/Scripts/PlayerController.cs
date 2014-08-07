e42a82c6f14115e56c78a99629b268001f476062
Robert <lum.robert@gmail.com>
2014-08-07 19:41:37 -0400

    saved scene one; added force to cube

---
+++ Game_One/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello World");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Declare force
+		Vector3 force;
+		force = new Vector3(10f, 0, 0);
+		//Instatiate force to a new instace of Vector3
+
+		rigidbody.AddForce (force);
+
+	}
+}
