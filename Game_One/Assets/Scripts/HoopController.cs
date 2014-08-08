fda54004814dde9077f13de7c1904c61f54c1d2f
Robert <lum.robert@gmail.com>
2014-08-07 21:00:31 -0400

    hoop controller; collision detection with basketball

---
+++ Game_One/Assets/Scripts/HoopController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoopController : MonoBehaviour {
+
+	public GameObject[] hoopParts;
+	public Vector3[] positions;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Player"){
+			if (other.gameObject.rigidbody.velocity.y < 0){
+				Debug.Log ("Score!");
+				foreach(GameObject g in hoopParts){
+					g.rigidbody.isKinematic = false;
+					g.rigidbody.AddExplosionForce(100f, other.transform.position, 4f);
+				}
+			}
+		}
+	}
+}
